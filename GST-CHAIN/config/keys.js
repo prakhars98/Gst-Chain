15f11eba1232dd579f52ef20e87f3261b4a9525f
prakhars98 <43076632+prakhars98@users.noreply.github.com>
2022-07-11 21:49:44 +0530

    Update keys.js

--- GST-CHAIN/config/keys.js
+++ GST-CHAIN/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI :'mongodb+srv://prakhar:prakhar@1998@gst-db-hvngq.mongodb.net/GST-DB?retryWrites=true&w=majority'
+    MongoURI :'your mongo URI displayed on the mongodb atlas platform'
 }
